b981eb666ac161f7cf5cead3442778784423a70a
Earl <earl.willis@publicismodem.com>
2015-03-03 20:22:39 -0500

    swifter write review

--- pepto/src/test/java/mobile/test/CharminMobileIphone.java
+++ pepto/src/test/java/mobile/test/CharminMobileIphone.java
@@ -35,7 +35,7 @@ public class CharminMobileIphone {
 	  public void run() throws Exception {
 		  String timeStamp = new SimpleDateFormat("MMdd_HHmmss").format(Calendar.getInstance().getTime());
 		  String local=(new java.io.File("").getAbsolutePath());
-			String data="" + local + "/" + "mobileCharmin.csv";	
+			String data="" + local + "/" + "cadi.csv";	
 			int row=1;
 		BufferedReader br = null;
 		String line = "";
@@ -45,7 +45,7 @@ public class CharminMobileIphone {
 		
 		
 		// @SuppressWarnings("deprecation")
-		WebDriver driver = new IPhoneDriver("http://192.168.2.2:3001/wd/hub/");
+		WebDriver driver = new IPhoneDriver("http://192.168.2.6:3001/wd/hub/");
 		
 		  //Write a review
 		  
@@ -61,28 +61,17 @@ public class CharminMobileIphone {
 				System.out.println("{Product [code= " + product[0] 
 	                                 + " , name=" + product[1] + "]");
 				 driver.get(product[0]);
-				 driver.findElement(By.linkText(product[1])).click();
+	
 				 driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 				 driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 				 driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 				 driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 				 driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-				 
-				 driver.findElement(By.linkText(product[3])).click();
-				 driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-				 driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-				 driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-				 driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-				 driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+				
 				 File scrFile1 = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-					FileUtils.copyFile(scrFile1, new File("Y://Screenshots/charmins/" + timeStamp +  "/" + row + "-" + timeStamp + "-" + "product.png"));
+					FileUtils.copyFile(scrFile1, new File("Y://Screenshots/cadi/" + timeStamp +  "/" + row + "-" + timeStamp + "-" + "cadilac.png"));
 					row+=1;
-					Select droplist = new Select(driver.findElement(By.id("phsmartphonebody_0_phsmartphoneproductprimarycontentarea_0_ddlQuantity")));   
-					droplist.selectByVisibleText(product[2]);
-					driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-					driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-					driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-					driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+					
 					driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 					
 					//	
@@ -92,38 +81,12 @@ public class CharminMobileIphone {
 						  //alert('The element was not found, as expected');
 					///	});
 					//((Rotatable) driver).rotate(ScreenOrientation.PORTRAIT);
-					driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-				 driver.findElement(By.linkText("Add To Cart")).click();
-				 File scrFile2 = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-					FileUtils.copyFile(scrFile2, new File("Y://Screenshots/charmins/" + timeStamp +  "/" + row + "-" + timeStamp + "-" + "cart.png"));
-					row+=1;
-					//((Rotatable) driver).rotate(ScreenOrientation.PORTRAIT);
-					driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+					
 					
 			}
-			row+=1;
-			driver.get("http://author.charmin.pgsitecore.com/en-us/shopping-cart");
-			driver.findElement(By.id("phsmartphonebody_0_phsmartphoneshoppingcartretailerlist_0_rptRetailerList_imgRetailerLogo_0"));
-			File scrFile3 = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-			FileUtils.copyFile(scrFile3, new File("Y://Screenshots/charmins/" + timeStamp +  "/" + row + "-" + timeStamp + "-" + "cart.png"));
-			row+=1;
-			driver.findElement(By.id("phsmartphonebody_0_phsmartphoneshoppingcartretailerlist_0_rptRetailerList_imgRetailerLogo_2"));
-			File scrFile4 = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-			FileUtils.copyFile(scrFile4, new File("Y://Screenshots/charmins/" + timeStamp +  "/" + row + "-" + timeStamp + "-" + "cart.png"));
-			row+=1;
-			driver.findElement(By.id("phsmartphonebody_0_phsmartphoneshoppingcartretailerlist_0_rptRetailerList_imgRetailerLogo_3"));
-			File scrFile5 = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-			FileUtils.copyFile(scrFile5, new File("Y://Screenshots/charmins/" + timeStamp +  "/" + row + "-" + timeStamp + "-" + "cart.png"));
-			row+=1;
-			driver.findElement(By.id("phsmartphonebody_0_phsmartphoneshoppingcartretailerlist_0_rptRetailerList_imgRetailerLogo_4"));
-			File scrFile6 = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-			FileUtils.copyFile(scrFile6, new File("Y://Screenshots/charmins/" + timeStamp +  "/" + row + "-" + timeStamp + "-" + "cart.png"));
-			row+=1;
-			driver.findElement(By.id("phsmartphonebody_0_phsmartphoneshoppingcartretailerlist_0_rptRetailerList_imgRetailerLogo_5"));
+		
 			
-			
-			File scrFile9 = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-			FileUtils.copyFile(scrFile9, new File("Y://Screenshots/charmins/" + timeStamp +  "/" + row + "-" + timeStamp + "-" + "cart.png"));
+
 			
 	 
 		} catch (FileNotFoundException e) {
